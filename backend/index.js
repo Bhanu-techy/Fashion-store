46d9967855341a106c59c693590cdef6a58e9c8a
Bhanu-techy <bhanuprakashdevari@gmail.com>
2026-05-20 17:34:03 +0530

    updated register api

--- backend/index.js
+++ backend/index.js
@@ -58,9 +58,14 @@ app.post("/register", async (req, res) => {
 
     await user.save();
 
-    await sendRegistrationEmail(email, name);
-
     res.status(200).json({token, user: {id: user.id, email: user.email,},});
+
+    try {
+      await sendRegistrationEmail(email, name);
+    } catch (err) {
+      res.json({msg: err.message});
+    }
+
     
   } catch (err) {
     res.status(500).json({ message: err.message });
