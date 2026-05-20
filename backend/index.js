007e25764630c41064b1285a83e6bd3a7a77b6a8
Bhanu-techy <bhanuprakashdevari@gmail.com>
2026-05-20 17:11:11 +0530

    updated register api

--- backend/index.js
+++ backend/index.js
@@ -63,7 +63,7 @@ app.post("/register", async (req, res) => {
     res.status(200).json({token, user: {id: user.id, email: user.email,},});
     
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ message: err.message });
   }
 });
 
